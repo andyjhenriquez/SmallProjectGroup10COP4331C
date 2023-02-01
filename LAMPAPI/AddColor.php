41293c174e1b1fe205ebad5d6fbd141c27e200c9
Andy Henriquez <andyjh1991@yahoo.com>
2023-02-01 16:48:06 +0000

    Basic API done for adding colors

---
+++ LAMPAPI/AddColor.php
@@ -0,0 +1,60 @@
+<?php
+	$inData = getRequestInfo();
+
+	var_dump($inData); // Print statement but for non-Strings
+
+	$colorName = $inData["name"];
+	$userID = $inData["userID"];
+
+	// Connect to MySQL with (localhost, username, password, database)
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		echo "Connection unsuccessful\n";
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		echo "Connection successful\n";
+
+		// Molds the skeleton for the action to be taken in the database
+		$stmt = $conn->prepare("INSERT INTO Colors (name, userID) VALUES(?,?)");
+		echo "Prepared\n";
+
+		// Sets variables (s for string i for integer)
+		$stmt->bind_param("si", $colorName, $userID);
+		echo "Bound\n";
+
+		// Executes SQL command
+		$stmt->execute(); 
+		echo "Executed\n";
+
+		$stmt->close();
+		echo "Closing stmt\n";
+
+		$conn->close();
+		echo "Closing the connection\n";
+		
+		returnWithError("");
+		echo "Returned with error\n";
+	}
+
+	// Translates the request into a json body
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
