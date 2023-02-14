3bf4f8eb9e4717ac5f6e48fe36da3c93066b8b1a
annbinus <annbinus@knights.ucf.edu>
2023-02-14 01:06:57 +0000

    Finalized UpdateContact endpoint

---
+++ LAMPAPI/UpdateContact.php
@@ -0,0 +1,49 @@
+<?php
+    ini_set('display_errors', 1);
+
+    $inData = getRequestInfo();
+
+    $contactID = $inData["ID"];
+
+	// Connect to MySQL with (localhost, username, password, database)
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		// Molds the skeleton for the action to be taken in the database
+		$stmt = $conn->prepare("UPDATE DevContacts SET firstName = ?, lastName = ?, phone = ?, email = ? WHERE ID=?");
+
+		// Sets variables (s for string i for integer)
+		$stmt->bind_param("ssssi", $inData['firstName'], $inData['lastName'], $inData['phone'], $inData['email'], $contactID);
+
+        // Executes SQL command
+		$stmt->execute();
+
+        $result = $stmt->get_result();
+
+		$stmt->close();
+		$conn->close();
+	}
+
+	// Translates the request into a json body
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
