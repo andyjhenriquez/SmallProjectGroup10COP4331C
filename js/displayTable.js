ab89293ac62e24d53bce7b372f74c5353e876d17
Curbeks <andyjh1991@yahoo.com>
2023-02-23 16:36:06 -0500

    reverted changes

--- js/displayTable.js
+++ js/displayTable.js
@@ -17,7 +17,13 @@ function displayData(dataFromTable)
         "bLengthChange": false, // Hides changing # of entries shown
         "sDom": 'lrtip', // Hides default search box
         "bInfo": false }); // Hides # of entries currently shown
-    
+      
+        // Allows for clicking on rows
+        DataTable = $('#dataTable').DataTable();
+          DataTable.on('click', 'tbody tr', function() {
+          console.log('API row values : ', DataTable.row(this).data());
+          idNum = DataTable.row(this).data().ID;
+      })
       // Allows for use of our own search input field
       $("#searchContact").keyup(function() {
           dataTable.fnFilter(this.value);
